d38169486615a040ca44fe3233f08491d69e2249
Jair Silva <luis.silor@gmail.com>
2023-06-01 14:37:26 -0600

    avances del front

--- js/script.js
+++ js/script.js
@@ -1,15 +1,50 @@
+
+function habilitarInput() {
+  var combo = document.getElementById("combo");
+  var input = document.getElementById("input");
+
+  if (combo.value === "casado") {
+    input.disabled = false;
+  } else {
+    input.disabled = true;
+  }
+}
+
+
+
+
+//funcion que me va mandar el contneid del fomrulario en un json
 document.getElementById('myForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Evitar el envío del formulario por defecto
 
     // Capturar los valores ingresados por el usuario
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+    const nss = document.getElementById('nss').value;
+    const fechaIng = document.getElementById('fechaIng').value;
+    const sexo = document.getElementById("sexo").value;
+    const combo= document.getElementById("combo").value;
+
+
+    comboSexo.addEventListener("change", function() {    
+      var seleccionado = comboSexo.value;
+    });
+
+    console.log(nss);
+    console.log(fecha_ing);
+    //verificar 
+    
+    
     // Construir el objeto JSON
     const data = {
-      name: name,
-      email: email
+      nss: nss,
+      fecha_ing: fecha_ing,
+      sexo: seleccionado,
+    
+
     };
-    // Enviar el JSON al servidor
+    //activar el input edad del conyuge si se selecciona la opcion de casado
+  
+
+ // Enviar el JSON al servidor
     fetch('url-del-servidor', {
       method: 'POST',
       headers: {
