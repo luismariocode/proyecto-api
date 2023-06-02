0ae9998ee4786ca472a680e78d748a4056702172
Jair Silva <luis.silor@gmail.com>
2023-06-02 15:50:27 -0600

    Avances del front se continua con la pantalla de resultados

--- js/script.js
+++ js/script.js
@@ -11,46 +11,46 @@ function habilitarInput() {
 }
 
 
-
-
 //funcion que me va mandar el contneid del fomrulario en un json
 document.getElementById('myForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Evitar el envío del formulario por defecto
 
     // Capturar los valores ingresados por el usuario
+
     const nss = document.getElementById('nss').value;
     const fechaIng = document.getElementById('fechaIng').value;
+    const fechaNac = document.getElementById('fechaNac').value
     const sexo = document.getElementById("sexo").value;
     const combo= document.getElementById("combo").value;
-
-
-    comboSexo.addEventListener("change", function() {    
-      var seleccionado = comboSexo.value;
-    });
-
-    console.log(nss);
-    console.log(fecha_ing);
-    //verificar 
-    
-    
-    // Construir el objeto JSON
+    const edadC = document.getElementById("input").value;
+    const sueldoCot = document.getElementById("sueldoCot").value;
+    const saldoInd = document.getElementById("saldoInd").value;
+  
+    // Construir el objeto JSON asi es e formato del jason que se va mandar el servidor con el fetch
     const data = {
       nss: nss,
-      fecha_ing: fecha_ing,
-      sexo: seleccionado,
+      fechaIng: fechaIng,
+      fechaNac: fechaNac,
+      Genero: sexo,
+      estado : combo,
+      edadC : edadC,
+      sueldoCot : sueldoCot,
+      saldoInd : saldoInd
+      
+    }
+
+    console.log(data);
+    //activar el input edad del conyuge si se selecciona la opcion de casado
+    
     
 
-    };
-    //activar el input edad del conyuge si se selecciona la opcion de casado
-  
-
- // Enviar el JSON al servidor
+    // Enviar el JSON al servidor
     fetch('url-del-servidor', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json; charset=UTF-8'
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data)  
     })
     .then(response => {
       // Manejar la respuesta del servidor
@@ -60,7 +60,35 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
       // Manejar errores
       console.error('Error:', error);
     });
-  });     
   
+
+    window.location.href = "resultados.html";
+  });     
+
+  
+  //codigo que se puede usar para esperar la respuesta del servidor
+
+   // Envía el JSON al servidor utilizando fetch
+//    fetch("URL_DEL_SERVIDOR", {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//   .then(function(response) {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error("Error en la respuesta del servidor.");
+//   })
+//   .then(function(responseJson) {
+//     // Aquí puedes procesar los resultados obtenidos del servidor y mostrarlos al usuario
+//     console.log(responseJson);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+// });
   
   
