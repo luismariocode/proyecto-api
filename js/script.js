34b37f6abbd8bac5aaf6f136cee6e50042e8f7e4
Jair Silva <luis.silor@gmail.com>
2023-06-21 16:48:00 -0600

    cambios en el formulario faltan los cambios en el formulario de resu8ltados

--- js/script.js
+++ js/script.js
@@ -3,15 +3,37 @@
 //activar el input edad del conyuge si se selecciona la opcion de casado
 
 function habilitarInput() {
-  var combo = document.getElementById("combo");
-  var input = document.getElementById("input");
-
+  var combo = document.getElementById("estadoC");
+  var input = document.getElementById("edadCon");
+  var label = document.getElementById("lbl-estadoC");
   if (combo.value === "casado") {
-    input.disabled = false;
+  
+    input.hidden = false;
+    label.hidden = false
   } else {
-    input.disabled = true;
+   
+    input.hidden = true;
+    label.hidden =true
+  
   }
 }
+
+var estimarIMSS;
+var estimarRJP;
+    
+function checkBoxClicked() {
+  var checkbox = document.getElementById("estimarIMSS");
+  var checkbox2 = document.getElementById("estimarRJP");
+
+  var result = checkbox.checked ? 1 : 0;
+  var result2 = checkbox2.checked ? 1 : 0;
+
+  estimarIMSS= result;
+  estimarRJP=result2; 
+  
+  // Aquí puedes hacer lo que desees con el resultado
+}
+
 
 //funcion en la ventana de resultados que me divide en 3 botones 
 
@@ -39,71 +61,68 @@ var semanasCotizadas;
 document.getElementById('myForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Evitar el envío del formulario por defecto
 
-    // Capturar los valores ingresados por el usuario
+    const semanasCot = document.getElementById("semanasCot").value
+    const fechaIng = document.getElementById("fechaIng").value
+    const edadActual = document.getElementById("edadActual").value
+    const genero = document.getElementById("sexo").value
+    const estadoCiv = document.getElementById("estadoC").value
+    const edadCon = document.getElementById("edadCon").value
+    const sueldoCot = document.getElementById("sueldoCot").value
+    const saldoIMSS =document.getElementById("saldoIMSS").value
+    const saldoRJP = document.getElementById("saldoRJP").value
+    // const estimarRJP = document.getElementById("estimarRJP").value
+    const infonavit = document.getElementById("comboInf").value
+    const tasa = document.getElementById("comboTasa").value
 
-    const semanasCot = document.getElementById('semanasCot').value;
-    const fechaIng = document.getElementById('fechaIng').value;
-    const fechaNac = document.getElementById('fechaNac').value
-    const sexo = document.getElementById("sexo").value;
-    const combo= document.getElementById("combo").value;
-    const edadC = document.getElementById("input").value;
-    const sueldoCot = document.getElementById("sueldoCot").value;
-    const saldoInd = document.getElementById("saldoInd").value;
-    const infonavit = document.getElementById("comboInf").value;
-    const tasa = document.getElementById("comboTasa").value;
+   
 
-    const estimar_uno= document.getElementById("estimar_uno").Checked;
-    const estimar_dos= document.getElementById("estimar_dos").Checked;
 
-    var estimarC;    
-    // verificar que opcion del chekbox selecciono el usuario
-    if (estimar_uno){
-      estimar= estimar_uno;
-    }
-    else{
-      estimar = estimar_dos;
-    }
-    // Construir el objeto JSON asi es e formato del json que se va mandar el servidor con el fetch
-
+    //construimos el objeto json el cual se va enviar a la API con post 
+    
     const data = {
-      
-      semanasCot: semanasCot, //semanas cotizadas ante el IMSS
-      fechaIng: fechaIng,     //fecha de ingreso como trabajador del IMSS
-      fechaNac: fechaNac,     // fecha de nacimiento 
-      genero: sexo,           //genero M , F
-      estado : combo,         //estado civil
-      edadC : edadC,          // Si se encuenta casa edad del conyuge
-      sueldoCot : sueldoCot,  //sueldo con el que cotiza al imss
-      saldoInd : saldoInd  ,  //saldo de la cuenta individual
-      infonavit : infonavit , //a usado se credito infonavit
-      tasa : tasa,            // a que tasa se va a realizar el calculo 
-      estimarC: estimar                                                                                                                                                                                                                             //estimar el saldo de tu cuenta individual
-
+      semanasCot : semanasCot,
+      fechaIng: fechaIng,
+      edadActual: edadActual,
+      genero :genero,
+      estadoCiv :estadoCiv,
+      edadCon:edadCon,
+      sueldoCot:sueldoCot,
+      saldoIMSS:saldoIMSS,
+      estimarIMSS:estimarIMSS,
+      saldoRJP:saldoRJP,
+      estimarRJP:estimarRJP,
+      infonavit:infonavit,
+      tasa:tasa
+                                                                                        
     }
+
     console.log(data);
    
       // semanasCotizadas=semanasCot;
       // document.getElementById("sc").textContent = params.get("semanasCot");
 
     // Enviar el JSON al servidor
-    fetch('http://192.168.1.235:8574/sntssrv.dll/api/rest/tsm/prueba', {
-      method: 'POST',
-      mode: 'no-cors', 
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      body: JSON.stringify(data)  
-    })
-    .then(response => {
-        // Manejar la respuesta del servidor
-      console.log('Respuesta del servidor:', response);
-    })
-    .catch(error => {
-      // Manejar errores
-      console.error('Error:', error);
-    });
+    // fetch('http://192.168.1.235:8574/sntssrv.dll/api/rest/tsm/prueba', {
+    //   method: 'GET',
+    //   mode: 'no-cors', 
+    //   headers: {
+    //     'Content-Type': 'application/json; charset=UTF-8'
+    //   },
+    //   body: JSON.stringify(data)  
+    // })
+    // .then(response => {
+    //     // Manejar la respuesta del servidor
+    //   console.log('Respuesta del servidor:', response);
+    // })
+    // .catch(error => {
+    //   // Manejar errores
+    //   console.error('Error:', error);
+    // });
     
     // window.location.href = "./api/prueba.html";
+
+
+    //terminacion del submit
   });     
 
 
@@ -113,37 +132,37 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
       // Hacer la distribucion para las tres tablas
 
 
-      var jsonResultados = "";
-      // Parsear el JSON el cual se va dividir en 3 difernetes tablas
-      var resultados = JSON.parse(jsonResultados);
-      // Obtener la referencia a la tabla
-      var tabla = document.getElementById('resultados-total');
-      // Recorrer los resultados y agregarlos a la tabla
-      resultados.forEach(function (resultado) {
-          var fila = document.createElement('tr');
+      // var jsonResultados = "";
+      // // Parsear el JSON el cual se va dividir en 3 difernetes tablas
+      // var resultados = JSON.parse(jsonResultados);
+      // // Obtener la referencia a la tabla
+      // var tabla = document.getElementById('resultados-total');
+      // // Recorrer los resultados y agregarlos a la tabla
+      // resultados.forEach(function (resultado) {
+      //     var fila = document.createElement('tr');
 
-          var celdaEdad = document.createElement('td');
-          celdaEdad.textContent = resultado.edad;
-          fila.appendChild(celdaEdad);
+      //     var celdaEdad = document.createElement('td');
+      //     celdaEdad.textContent = resultado.edad;
+      //     fila.appendChild(celdaEdad);
 
-          var celdaAntiguedad = document.createElement('td');
-          celdaAntiguedad.textContent = resultado.Antiguedad;
-          fila.appendChild(celdaAntiguedad);
+      //     var celdaAntiguedad = document.createElement('td');
+      //     celdaAntiguedad.textContent = resultado.Antiguedad;
+      //     fila.appendChild(celdaAntiguedad);
 
-          var celdaSueldoCot = document.createElement('td');
-          celdaSueldoCot.textContent = resultado.sueldoCot;
-          fila.appendChild(celdaSueldoCot);
+      //     var celdaSueldoCot = document.createElement('td');
+      //     celdaSueldoCot.textContent = resultado.sueldoCot;
+      //     fila.appendChild(celdaSueldoCot);
 
 
-          var celdaSaldoInd = document.createElement('td');
-          celdaSaldoInd.textContent = resultado.saldoInd;
-          fila.appendChild(celdaSaldoInd);
+      //     var celdaSaldoInd = document.createElement('td');
+      //     celdaSaldoInd.textContent = resultado.saldoInd;
+      //     fila.appendChild(celdaSaldoInd);
 
-          var celdaRenta = document.createElement('td');
-          celdaRenta.textContent = resultado.RentaVital;
-          fila.appendChild(celdaRenta);
-          tabla.appendChild(fila);
-      });
+      //     var celdaRenta = document.createElement('td');
+      //     celdaRenta.textContent = resultado.RentaVital;
+      //     fila.appendChild(celdaRenta);
+      //     tabla.appendChild(fila);
+      // });
 
 
   
